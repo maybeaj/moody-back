21c14ff92467b708d0e28785a189b1db749d0e49
이효진 <rabbit3456@naver.com>
2024-09-18 20:24:56 +0900

    controller, service 마무리 swagger 실행

---
+++ src/main/java/mood/diary/back/posts/ctrl/PostsController.java
@@ -0,0 +1,88 @@
+package mood.diary.back.posts.ctrl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import mood.diary.back.posts.domain.PostsRequestDTO;
+import mood.diary.back.posts.domain.PostsResponseDTO;
+import mood.diary.back.service.PostsService;
+
+@RestController
+@RequestMapping("mood")
+public class PostsController {
+
+    @Autowired
+    private PostsService postsService;
+    
+    // 목록 조회
+    @GetMapping("/list")
+    public ResponseEntity<List<PostsResponseDTO>> getList() {
+        System.out.println("debug >>> posts controller client path /list");
+        List<PostsResponseDTO> list = postsService.list();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    
+    // id 기준 데이터 조회
+    @GetMapping("/select/{id}")
+    public ResponseEntity<Object> select(@PathVariable Integer id) {
+        System.out.println("debug >>> posts controller client path /select");
+        System.out.println("debug >>> id param value " + id);
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("id", id);
+
+        PostsResponseDTO response = postsService.select(map);
+        if (response != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("게시글이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<PostsRequestDTO> save(PostsRequestDTO params) {
+        System.out.println("debug>>>> posts controller client path /post");
+        System.out.println(">>>>request dto, " + params);
+        postsService.save(params);
+
+        return new ResponseEntity<>(params, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Void> update(PostsRequestDTO params) {
+        System.out.println("debug >>> posts controller client path /update");
+        System.out.println(">>>>> request dto, " + params);
+        postsService.update(params);
+
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("delete/{id}")
+    @Operation(summary = "게시글 삭제", description = "게시글 키 값(id)을 가지고 삭제한다.")
+    public ResponseEntity<String> delete(@PathVariable Integer id) {
+        System.out.println("debug >>> posts controller client path /delete");
+        System.out.println("debug >>> id param value " + id);
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("id", id);
+
+        postsService.delete(map);
+        return new ResponseEntity<String>(id + "번째 데이터 삭제 ", HttpStatus.OK);
+    }
+}
--- src/main/java/mood/diary/back/service/PostsService.java
+++ src/main/java/mood/diary/back/service/PostsService.java
@@ -1,9 +1,13 @@
 package mood.diary.back.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mood.diary.back.posts.domain.PostsRequestDTO;
+import mood.diary.back.posts.domain.PostsResponseDTO;
 import mood.diary.back.posts.mapper.PostsMapper;
 
 @Service
@@ -20,5 +24,23 @@ public class PostsService {
     public void update(PostsRequestDTO params) {
         System.out.println("debug >>> service update" + postsMapper);
         postsMapper.updatePost(params);    
+    }
+
+    public void delete(Map<String, Integer> map) {
+        System.out.println("debug >>> service delete" + map.get("id"));
+        postsMapper.deletePost(map);    
+    }
+
+    public List<PostsResponseDTO> list(){
+        System.out.println("debug >>> service list");
+        return postsMapper.getPost();
+    }
+
+    public PostsResponseDTO select(Map<String, Integer> map){
+
+        PostsResponseDTO result = postsMapper.selectPost(map);
+        System.out.println("debug >>>> service result " + result);
+
+        return result; 
     }
 }
